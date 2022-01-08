222ff3f6341ebcabf2cd07468e90ff70e485f646
Amitesh Garg <amiteshchd@gmail.com>
2022-01-09 00:28:57 +0530

    Added Sol file

---
+++ Hotel_room.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.6.0;
+
+contract HotelRoom {
+    address payable public owner;
+    enum Statuses { Vacant, Occupied }
+    Statuses currentStatus;
+    uint public amount = 2 ether;
+
+    constructor() public{
+        owner = msg.sender;
+        currentStatus = Statuses.Vacant;
+        amount = 2 ether;
+    }
+
+    event Occupy(address _occupant, uint _value);
+
+    modifier onlyWhileVacant {
+        require(currentStatus == Statuses.Vacant, "Currently Occupied.");
+        _;
+    }
+
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount, "Not Enough Ether Provided.");
+        _;
+    }
+
+
+    function updateAmount(uint _amount) public {
+        amount = _amount;
+    }
+
+    receive() external payable onlyWhileVacant costs(amount) {
+        currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
+    }
+}
